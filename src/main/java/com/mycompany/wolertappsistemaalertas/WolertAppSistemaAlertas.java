8c71b0f37828c611ccab812ccaa915921dd1b4a2
Katherine Sofia Gutierrez Barliza <ksgutierrez@unicesar.edu.co>
2026-05-13 01:11:12 -0500

    feat: inicializar proyecto WolertApp

---
+++ src/main/java/com/mycompany/wolertappsistemaalertas/WolertAppSistemaAlertas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wolertappsistemaalertas;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class WolertAppSistemaAlertas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
