e77596f7f3104c23ac0425f2daad91860af89ff1
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-05-18 19:55:59 -0500

    feat: corregir ComunaService, BarrioService y RolUsuarioService

--- src/main/java/sistemagestion/service/BarrioService.java
+++ src/main/java/sistemagestion/service/BarrioService.java
@@ -4,10 +4,60 @@
  */
 package sistemagestion.service;
 
+import java.sql.SQLException;
+import java.util.List;
+import sistemagestion.dao.BarrioDAO;
+import sistemagestion.model.Barrio;
+import sistemagestion.util.Validador;
+
 /**
  *
  * @author Maria Cristina
  */
 public class BarrioService {
-    
+
+    private BarrioDAO dao;
+
+    public BarrioService() throws SQLException {
+        dao = new BarrioDAO();
+    }
+
+    public void insertar(Barrio b) throws SQLException {
+
+        Validador.validarObjeto(b);
+        Validador.validarCampoVacio(b.getNombre());
+        Validador.validarObjeto(b.getComuna());
+
+        dao.insertar(b);
+    }
+
+    public void actualizar(Barrio b) throws SQLException {
+
+        Validador.validarObjeto(b);
+        Validador.validarCampoVacio(b.getNombre());
+
+        dao.actualizar(b);
+    }
+
+    public List<Barrio> listar() throws SQLException {
+        return dao.listar();
+    }
+
+    public Barrio buscarPorId(int id) throws SQLException {
+
+        if (id <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return dao.buscarPorId(id);
+    }
+
+    public void eliminar(int id) throws SQLException {
+
+        if (id <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        dao.eliminar(id);
+    }
 }
