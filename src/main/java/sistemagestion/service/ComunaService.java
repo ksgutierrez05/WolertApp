0051270a6c1f178e03c8d195eab509a71863ef34
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-05-18 19:55:59 -0500

    feat: corregir ComunaService, BarrioService y RolUsuarioService

--- src/main/java/sistemagestion/service/ComunaService.java
+++ src/main/java/sistemagestion/service/ComunaService.java
@@ -4,10 +4,59 @@
  */
 package sistemagestion.service;
 
+import java.sql.SQLException;
+import java.util.List;
+import sistemagestion.dao.ComunaDAO;
+import sistemagestion.model.Comuna;
+import sistemagestion.util.Validador;
+
 /**
  *
  * @author Maria Cristina
  */
 public class ComunaService {
-    
+
+    private ComunaDAO dao;
+
+    public ComunaService() throws SQLException {
+        dao = new ComunaDAO();
+    }
+
+    public void insertar(Comuna c) throws SQLException {
+
+        Validador.validarObjeto(c);
+        Validador.validarCampoVacio(c.getNombre());
+
+        dao.insertar(c);
+    }
+
+    public Comuna buscarPorId(int id) throws SQLException {
+
+        if (id <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return dao.buscarPorId(id);
+    }
+
+    public List<Comuna> listar() throws SQLException {
+        return dao.listar();
+    }
+
+    public void actualizar(Comuna c) throws SQLException {
+
+        Validador.validarObjeto(c);
+        Validador.validarCampoVacio(c.getNombre());
+
+        dao.actualizar(c);
+    }
+
+    public void eliminar(int id) throws SQLException {
+
+        if (id <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        dao.eliminar(id);
+    }
 }
--- src/main/java/sistemagestion/service/RolUsuarioService.java
+++ src/main/java/sistemagestion/service/RolUsuarioService.java
@@ -4,10 +4,58 @@
  */
 package sistemagestion.service;
 
+import java.util.List;
+import sistemagestion.dao.RolUsuarioDAO;
+import sistemagestion.model.RolUsuario;
+import sistemagestion.util.Validador;
+
 /**
  *
  * @author Maria Cristina
  */
 public class RolUsuarioService {
-    
+
+    private RolUsuarioDAO dao;
+
+    public RolUsuarioService() {
+        dao = new RolUsuarioDAO();
+    }
+
+    public boolean insertar(RolUsuario r) {
+
+        Validador.validarObjeto(r);
+        Validador.validarCampoVacio(r.getNombre());
+
+        return dao.insertar(r.getIdRol(), r.getNombre());
+    }
+
+    public RolUsuario buscarPorId(int idRol) {
+
+        if (idRol <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return dao.buscarPorId(idRol);
+    }
+
+    public List<RolUsuario> listar() {
+        return dao.listar();
+    }
+
+    public boolean actualizar(RolUsuario r) {
+
+        Validador.validarObjeto(r);
+        Validador.validarCampoVacio(r.getNombre());
+
+        return dao.actualizar(r.getIdRol(), r.getNombre());
+    }
+
+    public boolean eliminar(int idRol) {
+
+        if (idRol <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return dao.eliminar(idRol);
+    }
 }
