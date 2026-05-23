6249b5bb5627751ab2b5dd20a9c446c64abe6f3a
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-05-23 15:47:05 -0500

    feat(model): crear AtencionAlerta y EstadoAtencionAlerta

---
+++ src/main/java/sistemagestion/model/AtencionAlerta.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemagestion.model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Maria Cristina
+ */
+public class AtencionAlerta {
+    
+    private int id_atencion;
+    Alerta alerta;
+    EstadoAtencionAlerta estado;
+    private LocalDateTime fechaatencion;
+    private String descripcion;
+    TipoArma tipoarma;
+    MedioTransporte mediotransporte;
+    private String observacion;
+
+    public int getId_atencion() {
+        return id_atencion;
+    }
+
+    public void setId_atencion(int id_atencion) {
+        this.id_atencion = id_atencion;
+    }
+
+    public Alerta getAlerta() {
+        return alerta;
+    }
+
+    public void setAlerta(Alerta alerta) {
+        this.alerta = alerta;
+    }
+
+    public LocalDateTime getFechaatencion() {
+        return fechaatencion;
+    }
+
+    public void setFechaatencion(LocalDateTime fechaatencion) {
+        this.fechaatencion = fechaatencion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public TipoArma getTipoarma() {
+        return tipoarma;
+    }
+
+    public void setTipoarma(TipoArma tipoarma) {
+        this.tipoarma = tipoarma;
+    }
+
+    public MedioTransporte getMediotransporte() {
+        return mediotransporte;
+    }
+
+    public void setMediotransporte(MedioTransporte mediotransporte) {
+        this.mediotransporte = mediotransporte;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
