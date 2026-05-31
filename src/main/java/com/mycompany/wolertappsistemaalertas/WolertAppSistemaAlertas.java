2c0f0c3ff2c4f1f0e7d059cfd2d2a5c806f9f822
maria cristina martinez hinojosa <mariacristinamartinez@unicesar.edu.co>
2026-05-30 22:22:56 -0500

    feat: crear EmailService

--- src/main/java/com/mycompany/wolertappsistemaalertas/WolertAppSistemaAlertas.java
+++ src/main/java/com/mycompany/wolertappsistemaalertas/WolertAppSistemaAlertas.java
@@ -1,16 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.wolertappsistemaalertas;
-
 
 import sistemagestion.view.LoginApp;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sistemagestion.service.EmailService;
 import sistemagestion.view.MapaView;
-
 
 /**
  *
@@ -18,9 +16,9 @@ import sistemagestion.view.MapaView;
  */
 public class WolertAppSistemaAlertas extends Application {
 
-           @Override
+    @Override
     public void start(Stage stage) {
-        
+
         LoginApp login = new LoginApp();
 
         Scene scene = new Scene(login.getView(), 1000, 650);
@@ -31,7 +29,24 @@ public class WolertAppSistemaAlertas extends Application {
         stage.show();
         // Temporalmente abre el mapa directo, sin login ni dashboard
         //new MapaView().mostrar();
+        
+        
+         boolean enviado = EmailService.enviarCorreo(
+            "mariamartinez.joseumar@gmail.com", // pon tu correo personal aquí
+            "🚨 Prueba WolertApp",
+            "<h2 style='color:#e53935'>¡Funciona!</h2><p>El sistema de notificaciones de WolertApp está activo.</p>"
+    );
+
+    if (enviado) {
+    System.out.println("✅ Correo enviado correctamente");
+    } else {
+    System.out.println("❌ Error al enviar");
+      }  
     }
+
+    
+    
+    
 
     public static void main(String[] args) {
         launch(args);
